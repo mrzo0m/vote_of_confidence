ef67fd0cbad274e7cc867775f16e7036010d26be
Олег Буршинов <mrzoom.214@gmail.com>
2019-02-17 22:01:16 +0300

    Init modules

---
+++ communications/src/main/java/io/voteofconf/communications/CommunicationsApplication.java
@@ -0,0 +1,13 @@
+package io.voteofconf.communications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationsApplication.class, args);
+	}
+
+}
