1bc89f163b5ca726335085672ca7053c72519163
Burshinov.O.Al <Burshinov.O.Al@sberbank.ru>
2019-02-20 11:49:23 +0300

    Okta fix

--- communications/src/main/java/io/voteofconf/communications/CommunicationsApplication.java
+++ communications/src/main/java/io/voteofconf/communications/CommunicationsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@EnableDiscoveryClient
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
 )
