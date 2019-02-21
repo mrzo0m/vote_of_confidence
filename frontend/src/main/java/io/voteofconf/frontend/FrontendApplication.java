4f9c0973c530d25cf493a4a3d32b546c83f846b1
Burshinov.O.Al <Burshinov.O.Al@sberbank.ru>
2019-02-21 14:43:01 +0300

    Some stuff for use eureka in gateway

--- frontend/src/main/java/io/voteofconf/frontend/FrontendApplication.java
+++ frontend/src/main/java/io/voteofconf/frontend/FrontendApplication.java
@@ -1,10 +1,20 @@
 package io.voteofconf.frontend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
 		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
@@ -15,4 +25,17 @@ public class FrontendApplication {
 		SpringApplication.run(FrontendApplication.class, args);
 	}
 
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
 }
--- gateway/src/main/java/io/voteofconf/gateway/GatewayApplication.java
+++ gateway/src/main/java/io/voteofconf/gateway/GatewayApplication.java
@@ -2,12 +2,18 @@ package io.voteofconf.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication(exclude = {
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
 }
