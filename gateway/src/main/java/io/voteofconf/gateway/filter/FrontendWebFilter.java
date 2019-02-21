b7bfcdd397517d758c78cdf91149bb4c0118f571
Burshinov.O.Al <Burshinov.O.Al@sberbank.ru>
2019-02-21 19:02:10 +0300

    Zuul Die, Cloud Gateway is here! Filter

---
+++ gateway/src/main/java/io/voteofconf/gateway/filter/FrontendWebFilter.java
@@ -0,0 +1,19 @@
+package io.voteofconf.gateway.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class FrontendWebFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        if (exchange.getRequest().getURI().getPath().equals("/")) {
+            return chain.filter(exchange.mutate().request(exchange.getRequest().mutate().path("frontend/index.html").build()).build());
+        }
+
+        return chain.filter(exchange);
+    }
+}
