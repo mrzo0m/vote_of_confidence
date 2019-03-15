840d6bbab288dfb1d715023e922d2de3e5596427
entrix <entrix@github.com>
2019-03-15 18:17:23 +0300

    Add uri printing

--- gateway/src/main/java/io/voteofconf/gateway/controller/FallbackController.java
+++ gateway/src/main/java/io/voteofconf/gateway/controller/FallbackController.java
@@ -3,14 +3,26 @@ package io.voteofconf.gateway.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.gateway.route.RouteDefinition;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("fallback")
 public class FallbackController {
 
+    @Autowired
+    private DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator;
+
+    @Bean
+    public FallbackController(DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator) {
+        this.discoveryClientRouteLocator = discoveryClientRouteLocator;
+    }
+
     @GetMapping("frontend")
     public Mono<String> getFrontendFallback() {
+        for (RouteDefinition rd: discoveryClientRouteLocator.getRouteDefinitions()) {
+            System.out.println(rd.getId() + " -> " + rd.getUri());
+        }
         return Mono.just("Frontend temporarily unavailable. Please try again later!");
     }
 
