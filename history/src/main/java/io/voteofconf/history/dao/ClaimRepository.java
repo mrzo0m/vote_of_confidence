4c2f3745667583bfe4d87debfe94122e106aa943
Олег Буршинов <mrzoom.214@gmail.com>
2019-03-25 19:35:50 +0300

    swagger feature for webflux

--- history/src/main/java/io/voteofconf/history/dao/ClaimRepository.java
+++ history/src/main/java/io/voteofconf/history/dao/ClaimRepository.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @Repository
 public interface ClaimRepository extends ReactiveCassandraRepository<Claim, ClaimKey> {
 
     Flux<Claim> findByKeyCompanyName(final String companyName);
 
     Mono<Claim> findOneByKeyCompanyName(final String companyName);
+
+    Mono<Claim> findOneByKeyId(final UUID id);
 }
