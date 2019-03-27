6e4941f88c5bd53283fd58b40b1602c40285a2e4
Олег Буршинов <mrzoom.214@gmail.com>
2019-03-27 15:46:41 +0300

    Rest api for claim update

--- history/src/main/java/io/voteofconf/history/controller/ClaimController.java
+++ history/src/main/java/io/voteofconf/history/controller/ClaimController.java
@@ -2,17 +2,29 @@ package io.voteofconf.history.controller;
 
 import io.swagger.annotations.ApiOperation;
 import io.voteofconf.history.dao.Claim;
+import io.voteofconf.history.dao.ClaimKey;
 import io.voteofconf.history.dao.ClaimRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.UUID;
 
+/*
+ *  Best Practice for api:
+ *   /farmers
+ *   /farmers/{farmer_id}
+ *   /farmers/{farmer_id}/arrivals
+ *   /farmers/{farmer_id}/arrivals/{id}
+ *   /crops
+ *   /crops/{crop_id}
+ */
 @RestController
-@RequestMapping("claim")
+@RequestMapping("claims")
 public class ClaimController {
 
     private ClaimRepository claimRepository;
@@ -22,8 +34,14 @@ public class ClaimController {
         this.claimRepository = claimRepository;
     }
 
-    @GetMapping("/company/{name}")
-    public Mono<Claim> getClaimByCompany(@PathVariable(value = "name") String companyName) {
+
+    @GetMapping
+    public Flux<Claim> getAll() {
+        return claimRepository.findAll();
+    }
+
+    @GetMapping("/{company}")
+    public Mono<Claim> getClaimByCompany(@PathVariable(value = "company") String companyName) {
         return claimRepository.findOneByKeyCompanyName(companyName);
     }
 
@@ -31,6 +49,19 @@ public class ClaimController {
     @GetMapping("/{id}")
     public Mono<Claim> get(@PathVariable(value = "id") UUID id) {
         return claimRepository.findOneByKeyId(id);
+    }
+
+
+    @PutMapping
+    public Mono<Void> put() {
+        claimRepository.saveAll(Arrays.asList(
+                new Claim(new ClaimKey("Company"+UUID.randomUUID(), UUID.randomUUID()), "info: " + System.nanoTime()),
+                new Claim(new ClaimKey("Company"+UUID.randomUUID(), UUID.randomUUID()), "info: " + System.nanoTime()),
+                new Claim(new ClaimKey("Company"+UUID.randomUUID(), UUID.randomUUID()), "info: " + System.nanoTime()),
+                new Claim(new ClaimKey("Company"+UUID.randomUUID(), UUID.randomUUID()), "info: " + System.nanoTime()),
+                new Claim(new ClaimKey("Company"+UUID.randomUUID(), UUID.randomUUID()), "info: " + System.nanoTime())
+        )).subscribe();
+        return Mono.empty();
     }
 
 
