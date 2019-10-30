1c859eaf34dd0221e3d478b1e5f37e610c58f11f
Aleksei Volkov <entrix@github.com>
2019-10-31 01:13:30 +0300

    FIx again

--- gateway/src/main/java/io/voteofconf/gateway/GatewayApplication.java
+++ gateway/src/main/java/io/voteofconf/gateway/GatewayApplication.java
@@ -15,7 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
         DataSourceAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
         })
 public class GatewayApplication {
