b899a24d7366b5f48b6d5bfdef806f614cbe93fd
Олег Буршинов <mrzoom.214@gmail.com>
2019-11-28 00:50:02 +0300

    WireMock init

--- gateway/src/test/java/io/voteofconf/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/io/voteofconf/gateway/GatewayApplicationTests.java
@@ -1,27 +1,53 @@
 package io.voteofconf.gateway;
 
+import io.voteofconf.gateway.controller.FallbackController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebFluxTest(controllers = FallbackController.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(value = "test")
+@WithMockUser
+@AutoConfigureWireMock(port = 0)
 public class GatewayApplicationTests {
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
     public void contextLoads() {
-        assert (webTestClient != null);
-//        webTestClient
-//                .get().uri("/frontend") // GET method and URI
-//                .accept(MediaType.TEXT_PLAIN) //setting ACCEPT-Content
-//                .exchange() //gives access to response
-//                .expectStatus().isOk() //checking if response is OK
-//                .expectBody(String.class).isEqualTo("Hello World!"); // checking
+        stubFor(get(
+                urlEqualTo("/history-microservice"))
+                .willReturn(
+                        aResponse()
+                                .withHeader(
+                                        HttpHeaders.CONTENT_TYPE,
+                                        MediaType.APPLICATION_JSON_VALUE)
+                                .withBody("body")
+                                .withStatus(HttpStatus.OK.value())));
+
+        webTestClient
+                .get()
+                .uri("/history-microservice")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .expectBody();
+
     }
 
 }
