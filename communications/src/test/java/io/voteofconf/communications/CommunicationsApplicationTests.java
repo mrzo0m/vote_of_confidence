59a1d95324f61e569e9c2c49c8f828031dc59717
Олег Буршинов <mrzoom.214@gmail.com>
2019-11-28 15:09:21 +0300

    WireMock init work

--- communications/src/test/java/io/voteofconf/communications/CommunicationsApplicationTests.java
+++ communications/src/test/java/io/voteofconf/communications/CommunicationsApplicationTests.java
@@ -5,17 +5,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.test.StepVerifier;
 
-import java.util.function.Predicate;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WithMockUser
 @ActiveProfiles(value = "test")
+@AutoConfigureWireMock(port = 8000)
 public class CommunicationsApplicationTests {
 
     @Autowired
@@ -24,10 +29,19 @@ public class CommunicationsApplicationTests {
 
     @Test
     public void contextLoads() {
+        stubFor(get(
+                urlEqualTo("/history-microservice"))
+                .willReturn(
+                        aResponse()
+                                .withHeader(
+                                        HttpHeaders.CONTENT_TYPE,
+                                        MediaType.APPLICATION_JSON_VALUE)
+                                .withBody("[{\"inviteName\":\"Oleg\"}]")
+                                .withStatus(HttpStatus.OK.value())));
 
         StepVerifier
                 .create(this.backlogClient.getAllBacklog())
-                .expectNextMatches(Predicate.isEqual(null))
+                .expectNextMatches(backlog -> backlog != null)
                 .verifyComplete();
     }
 
