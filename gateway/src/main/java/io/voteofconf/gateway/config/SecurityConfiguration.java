e88f4f6d940752583023734f7e46d8eb57991477
a.v.volkov <Kraal321>
2019-12-12 22:49:22 +0300

    completely unsecure gateway

--- gateway/src/main/java/io/voteofconf/gateway/config/SecurityConfiguration.java
+++ gateway/src/main/java/io/voteofconf/gateway/config/SecurityConfiguration.java
@@ -1,34 +1,34 @@
 package io.voteofconf.gateway.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
 
 //@Configuration
 //@EnableWebFluxSecurity
 //@EnableReactiveMethodSecurity
 public class SecurityConfiguration {
 
-//    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
+////    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        return http
+////                .authorizeExchange()
+////                .pathMatchers("/**")
+////                .permitAll()
+//////                      .and()
+//////                .oauth2Login()
+////                .and()
+//                .oauth2ResourceServer()
+//                .jwt().and().and()
 //                .authorizeExchange()
-//                .pathMatchers("/**")
-//                .permitAll()
-////                      .and()
-////                .oauth2Login()
+//                .anyExchange()
+//                .authenticated()
 //                .and()
-                .oauth2ResourceServer()
-                .jwt().and().and()
-                .authorizeExchange()
-                .anyExchange()
-                .authenticated()
-                .and()
-                .build();
-    }
+//                .build();
+//    }
 
 //    @Bean
 //    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
