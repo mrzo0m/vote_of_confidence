6937fc8ad794594211584f944502388732170628
a.v.volkov <Kraal321>
2019-12-12 22:15:04 +0300

    unsecure gateway

--- gateway/src/main/java/io/voteofconf/gateway/config/SecurityConfiguration.java
+++ gateway/src/main/java/io/voteofconf/gateway/config/SecurityConfiguration.java
@@ -7,9 +7,9 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-@EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
+//@Configuration
+//@EnableWebFluxSecurity
+//@EnableReactiveMethodSecurity
 public class SecurityConfiguration {
 
     @Bean
