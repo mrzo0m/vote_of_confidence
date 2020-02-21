850b4d8e0887f416fcf254cd39784e0ca831c899
Burshinov.O.Al <Burshinov.O.Al@sberbank.ru>
2020-02-21 15:59:04 +0300

    ssl to gateway

--- history/src/main/java/io/voteofconf/history/config/SecurityConfiguration.java
+++ history/src/main/java/io/voteofconf/history/config/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -11,7 +13,14 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Profile("!dev")
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.requiresChannel().anyRequest().requiresSecure();
+    }
 
     @Profile("!dev")
     @Bean
@@ -19,11 +28,6 @@ public class SecurityConfiguration {
         return http
                 .csrf().disable()
                 .cors().disable()
-//                .authorizeExchange()
-//                .anyExchange().authenticated()
-//                .and()
-//                .oauth2ResourceServer()
-//                .jwt().and()
                 .authorizeExchange()
                 .anyExchange().permitAll().and().build();
     }
